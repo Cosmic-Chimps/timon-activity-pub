bc6a14665e71a9fe48473a7b9774fdc962a6b7db
Puck Meerburg <puck@puckipedia.com>
2017-11-19 21:15:30 +0100

    Server: fix background task crashing due to race condition

--- Kroeg.Server/BackgroundTasks/BackgroundTaskQueuer.cs
+++ Kroeg.Server/BackgroundTasks/BackgroundTaskQueuer.cs
@@ -61,7 +61,8 @@ namespace Kroeg.Server.BackgroundTasks
                     {
                         if (sleepTime > 0)
                             _cancellationTokenSource.CancelAfter(sleepTime);
-                        _notifier.Subscribe(BackgroundTaskPath, (a) => _cancellationTokenSource?.Cancel());
+
+                        await _notifier.Subscribe(BackgroundTaskPath, (a) => _cancellationTokenSource?.Cancel());
                         await _connection.WaitAsync(_cancellationTokenSource.Token);
                     }
                     catch (OperationCanceledException) { }
